65a99748ffa594f2161ab8dc06dd66dafe9679c8
Tushar Sharma <sharma.tushar.271003@gmail.com>
2024-11-19 21:38:34 +0530

    updated nav bar styling

--- client/src/crawler/standalone.js
+++ client/src/crawler/standalone.js
@@ -1,24 +1,24 @@
 const axios = require('axios')
 
-// exports.urbancompany = async () => {
-//     let jobs = await axios.post('https://www.urbanclap.com/api/v2/org-operations/getAlljobs')
-//     console.log(jobs);
-//     jobs = jobs.data.jobs.map((job) => {
-//         return {
-//             job_id: job.job_id,
-//             title: job.job_title,
-//             description: job.jdDisplay,
-//             location: job.location[0],
-//             company: 'urbancompany',
-//             platform: 'standalone',
-//             apply_url: `https://careers.urbancompany.com/jobDetail?id=${job.job_id}`,
-//             created_at: job.job_created_timestamp,
-//             update_at: job.job_updated_timestamp,
-//         }
-//     })
+exports.urbancompany = async () => {
+    let jobs = await axios.post('https://www.urbanclap.com/api/v2/org-operations/getAlljobs')
+    console.log(jobs);
+    jobs = jobs.data.jobs.map((job) => {
+        return {
+            job_id: job.job_id,
+            title: job.job_title,
+            description: job.jdDisplay,
+            location: job.location[0],
+            company: 'urbancompany',
+            platform: 'standalone',
+            apply_url: `https://careers.urbancompany.com/jobDetail?id=${job.job_id}`,
+            created_at: job.job_created_timestamp,
+            update_at: job.job_updated_timestamp,
+        }
+    })
     
-//     return jobs;
-// }
+    return jobs;
+}
 
 exports.tata1mg = async () => {
     let jobs = await axios.get('https://www.1mg.com/pharmacy_api/get-all-jobs')
